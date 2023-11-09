35e44688d7e9add1dd42133fa01a469fc4374c19
Jason McLeod <jason@mcleods.me>
2023-11-08 20:07:35 -0500

    Update to version 1.19.2]

---
+++ launch.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -x
+
+FORGE_VERSION=1.19.2-43.3.2
+cd /data
+
+if ! [[ "$EULA" = "false" ]] || grep -i true eula.txt; then
+	echo "eula=true" > eula.txt
+else
+	echo "You must accept the EULA by in the container settings."
+	exit 9
+fi
+
+if ! [[ -f 'LITV3-1.19.2-Serverpack-2.4.zip' ]]; then
+	rm -fr config kubejs libraries mods *SERVER.zip forge*.jar
+	curl -Lo 'LITV3-1.19.2-Serverpack-2.4.zip' 'https://edge.forgecdn.net/files/4819/479/LITV3-1.19.2-Serverpack-2.4.zip' && unzip -u -o 'LITV3-1.19.2-Serverpack-2.4.zip' -d /data
+	DIR_TEST=$(find . -type d -maxdepth 1 | tail -1 | sed 's/^.\{2\}//g')
+	if [[ $(find . -type d -maxdepth 1 | wc -l) -gt 1 ]]; then
+		cd "${DIR_TEST}"
+		mv -f * /data
+		cd /data
+		rm -fr "$DIR_TEST"
+	fi
+  curl -Lo forge-${FORGE_VERSION}-installer.jar 'https://maven.minecraftforge.net/net/minecraftforge/forge/'${FORGE_VERSION}'/forge-'${FORGE_VERSION}'-installer.jar'
+	java -jar $(ls forge-*-installer.jar) --installServer && rm -f forge-*-installer.jar
+	echo "eula=true" > eula.txt
+fi
+
+if [[ -n "$JVM_OPTS" ]]; then
+	sed -i '/-Xm[s,x]/d' user_jvm_args.txt
+	for j in ${JVM_OPTS}; do sed -i '$a\'$j'' user_jvm_args.txt; done
+fi
+if [[ -n "$MOTD" ]]; then
+    sed -i "/motd\s*=/ c motd=$MOTD" /data/server.properties
+fi
+if [[ -n "$LEVEL" ]]; then
+    sed -i "/level-name\s*=/ c level-name=$LEVEL" /data/server.properties
+fi
+if [[ -n "$OPS" ]]; then
+    echo $OPS | awk -v RS=, '{print}' > ops.txt
+fi
+if [[ -n "$ALLOWLIST" ]]; then
+    echo $ALLOWLIST | awk -v RS=, '{print}' > white-list.txt
+fi
+
+sed -i 's/server-port.*/server-port=25565/g' server.properties
+
+chmod +x run.sh
+./run.sh
