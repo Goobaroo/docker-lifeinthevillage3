851cda682e5bc9b3ad3a71cf818a689974ad57b0
Jason McLeod <jason@mcleods.me>
2024-04-25 13:54:32 -0400

    Update to version 1.19.2]

--- launch.sh
+++ launch.sh
@@ -12,9 +12,9 @@ else
 	exit 9
 fi
 
-if ! [[ -f 'LITV3-1.19.2-Serverpack-2.7b.zip' ]]; then
+if ! [[ -f 'LITV3-1.19.2-Serverpack-2.8.zip' ]]; then
 	rm -fr config kubejs libraries mods *SERVER.zip forge*.jar
-	curl -Lo 'LITV3-1.19.2-Serverpack-2.7b.zip' 'https://edge.forgecdn.net/files/5177/639/LITV3-1.19.2-Serverpack-2.7b.zip' && unzip -u -o 'LITV3-1.19.2-Serverpack-2.7b.zip' -d /data
+	curl -Lo 'LITV3-1.19.2-Serverpack-2.8.zip' 'https://edge.forgecdn.net/files/5290/323/LITV3-1.19.2-Serverpack-2.8.zip' && unzip -u -o 'LITV3-1.19.2-Serverpack-2.8.zip' -d /data
 	DIR_TEST=$(find . -type d -maxdepth 1 | tail -1 | sed 's/^.\{2\}//g')
 	if [[ $(find . -type d -maxdepth 1 | wc -l) -gt 1 ]]; then
 		cd "${DIR_TEST}"
